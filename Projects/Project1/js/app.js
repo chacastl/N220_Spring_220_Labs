2127b1bab9d3d8870436460eee83b2b56aa98586
Charlie Castle <charliecastle2001@gmail.com>
2022-01-17 00:55:41 -0500

    updated drawing

--- Projects/Project1/js/app.js
+++ Projects/Project1/js/app.js
@@ -1,9 +1,34 @@
 function setup()
-{
- createCanvas(1000,1000);
- fill(11,222,333);
- stroke(11,222,333);
- circle(200,200,200);
- stroke(11,222,333);
- rect(400,200,2,2)
-}
+    {
+     createCanvas(800,600);
+    }
+    let x = 100;
+    let y = 100;
+    function draw()
+    {
+    
+    //main body   
+    fill(242,242,0);
+     stroke(242,242,0)
+     circle(200,200,200);
+     //eye 1
+     fill("black");
+     stroke("black");
+     circle(160,180,20);
+     fill("white");
+     stroke("white");
+     circle(160,180,10);
+    //eye 2
+     fill("black");
+     stroke("black");
+     circle(240,180,20);
+     fill("white");
+     stroke("white");
+     circle(240,180,10);
+     noFill();
+     stroke("Black");
+     strokeWeight(6);
+     arc(200, 250, 50, 50, 0, PI);
+     
+    
+    }
